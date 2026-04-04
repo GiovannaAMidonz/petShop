d0267b1cf0f061e3e2ec03243ffa7b566a3306af
Gio-Midonz-Bots <gmidonz09@gmail.com>
2026-04-03 22:49:15 -0300

    class animal

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,19 @@
 package org.example;
 
+import org.example.model.Animal;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        int option = 1;
+        Animal animal = new Animal();
+        animal.setEspecie("Gato");
+        animal.setID(1);
+        animal.setNome("Simba");
+        animal.setRaca("viralata");
 
+        System.out.println(animal.toString());
 
     }
 }
